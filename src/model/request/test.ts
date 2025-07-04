2c7c7eeecff9da3d2bd8ad131cfd81720e30378a
Max <79105210+replay1121@users.noreply.github.com>
2025-07-04 22:04:58 +0700

    Add Test interface to request model

---
+++ src/model/request/test.ts
@@ -0,0 +1,11 @@
+export interface Test {
+    msisdn: string;
+    message: string;
+    sender: string;
+    scheduled_delivery: string;
+    force: string;
+    Shorten_url: boolean;
+    tracking_url: string;
+    expire: string;
+  }
+  
